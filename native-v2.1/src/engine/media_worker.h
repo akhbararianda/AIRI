bd63f6ee708187948f240f013d35f901daf8aa26
akhbararianda <31719712+akhbararianda@users.noreply.github.com>
2026-08-18 11:05:40 +0700

    Add realtime media telemetry fields

--- native-v2.1/src/engine/media_worker.h
+++ native-v2.1/src/engine/media_worker.h
@@ -5,7 +5,11 @@
 #include <string>
 namespace airi {
 struct MediaRequest{std::string page_url,title,quality="best";std::filesystem::path save_directory;std::string cookie,referer,user_agent;};
-struct MediaProgress{int percent{};std::string status,detail;};
+struct MediaProgress{
+    int percent{};
+    std::string status,detail;
+    std::string downloaded_text,total_text,speed_text,eta_text,fragment_text;
+};
 class MediaWorker{public:using Callback=std::function<void(const MediaProgress&)>;static bool available();static bool download(const MediaRequest&,Callback);static std::filesystem::path tool_path();static std::filesystem::path tools_directory();};
 }
 #endif
